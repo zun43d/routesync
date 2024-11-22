db65192a3279455f00efd6a45321be54f6c7664b
Zunaed Ahmed <65343827+zun43d@users.noreply.github.com>
2024-11-22 20:47:15 +0000

    Login page done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+import { Inter as FontSans } from 'next/font/google'
+
+import { cn } from '@/lib/utils'
+
+const fontSans = FontSans({
+	subsets: ['latin'],
+	variable: '--font-sans',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<main className={cn('font-sans antialiased', fontSans.variable)}>
+			<Component {...pageProps} />
+		</main>
+	)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
+
+import { LoginForm } from '@/components/login-form'
+
 export default function Home() {
 	return (
-		<div>
-			<h1>RouteSync</h1>
-			<p>Transportation tracking and management system</p>
+		<div className="h-screen flex flex-col justify-center">
+			<header className="flex items-center justify-center pb-20 py-5 px-5 gap-2">
+				<Image src="/logo.svg" alt="RouteSync Logo" width={50} height={50} />
+				<h1 className="text-2xl font-semibold">RouteSync</h1>
+			</header>
+			<div className="flex items-center justify-center px-4">
+				<LoginForm />
+			</div>
 		</div>
 	)
 }
